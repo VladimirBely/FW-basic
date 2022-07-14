3fa0e5e31c7f9ccdc16736282054bcf5cd08e24a
VladimirBely <vladimirbely983@gmail.com>
2022-07-14 20:14:43 +0300

    hamburger

---
+++ js/hamburger-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#hamburger-btn").click(function () {
+    $("#nav-menu").css('display', 'block');
+  });
+
+  $("#close-btn").click(function () {
+    $("#nav-menu").css('display', 'none');
+  });
+});
+
