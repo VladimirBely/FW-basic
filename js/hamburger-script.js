f1bdf6f3dd9643ffe010a08127892461b4c8ed5e
VladimirBely <vladimirbely983@gmail.com>
2022-07-16 00:01:26 +0300

    btn action

--- js/hamburger-script.js
+++ js/hamburger-script.js
@@ -69,3 +69,23 @@ $(document).ready(function () {
     })
 });
 
+$(document).ready(function () {
+ $('#publication-btn-homepage').click(function(){
+  $('#publications-container-homepage').css('height', 'auto')
+ })
+ .click(function(){
+  $('#publication-btn-homepage').css('display', 'none')
+ })
+ .click(function(){
+  $('#close-btn-homepage').css('display', 'block')
+ })
+ $('#close-btn-homepage').click(function(){
+  $('#publications-container-homepage').css('height', '')
+ })
+ .click(function(){
+  $('#close-btn-homepage').css('display', 'none')
+ })
+ .click(function(){
+  $('#publication-btn-homepage').css('display', 'block')
+ })
+});
