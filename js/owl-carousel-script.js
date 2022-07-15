217d2c2c598ab4c82d5b7826fcd14e5dc96ef46c
VladimirBely <vladimirbely983@gmail.com>
2022-07-15 15:21:21 +0300

    carousel

---
+++ js/owl-carousel-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#main-carousel").owlCarousel({
+      items: 1,
+    //   autoplay: true,
+    //   loop: true,
+      responsiveRefreshRate: 50,
+    });
+  });
