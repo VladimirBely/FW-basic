5a1ff6dc8a2e7d9f3c78a8e89593db4df1d8e36b
VladimirBely <vladimirbely983@gmail.com>
2022-07-15 20:49:30 +0300

    experiment

--- js/hamburger-script.js
+++ js/hamburger-script.js
@@ -8,3 +8,64 @@ $(document).ready(function () {
   });
 });
 
+
+$(document).ready(function () {
+  $('#E-type2').click(function () {
+    $('#E-type2').addClass('E-type-active')
+  })
+    .click(function () {
+      $('#E-type1').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type3').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type4').removeClass('E-type-active')
+    })
+});
+
+$(document).ready(function () {
+  $('#E-type1').click(function () {
+    $('#E-type1').addClass('E-type-active')
+  })
+    .click(function () {
+      $('#E-type2').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type3').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type4').removeClass('E-type-active')
+    })
+});
+
+$(document).ready(function () {
+  $('#E-type3').click(function () {
+    $('#E-type3').addClass('E-type-active')
+  })
+    .click(function () {
+      $('#E-type1').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type2').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type4').removeClass('E-type-active')
+    })
+});
+
+$(document).ready(function () {
+  $('#E-type4').click(function () {
+    $('#E-type4').addClass('E-type-active')
+  })
+    .click(function () {
+      $('#E-type1').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type2').removeClass('E-type-active')
+    })
+    .click(function () {
+      $('#E-type3').removeClass('E-type-active')
+    })
+});
+
--- js/owl-carousel-script.js
+++ js/owl-carousel-script.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
-    $("#main-carousel").owlCarousel({
-      items: 1,
-      autoplay: true,
-      loop: true,
-      smartSpeed: 800,
-      autoplayTimeout: 2000,
-      responsiveRefreshRate: 50,
-    });
+  $("#main-carousel").owlCarousel({
+    items: 1,
+    autoplay: true,
+    loop: true,
+    smartSpeed: 800,
+    autoplayTimeout: 2000,
+    responsiveRefreshRate: 50,
   });
+});
+
+$(document).ready(function () {
+  $("#e-carousel").owlCarousel({
+    items: 1,
+    autoplay: true,
+    loop: true,
+    smartSpeed: 800,
+    autoplayTimeout: 2000,
+    responsiveRefreshRate: 50,
+  });
+});
